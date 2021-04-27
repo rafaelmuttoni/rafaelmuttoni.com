61b53cc0290e07567acca198fc169c83f2691ae8
Rafael Muttoni <hi@rafaelmuttoni.com>
2021-04-26 22:48:26 -0300

    changed theme colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,10 +3,9 @@ import { extendTheme } from '@chakra-ui/react'
 // 2. Call `extendTheme` and pass your custom values
 const theme = extendTheme({
   colors: {
-    brand: {
-      main: '#bd93f9',
-      // ...
-      secondary: '#6272a4',
+    theme: {
+      main: '#0D80BF',
+      secondary: '#0c70a6',
     },
   },
 })
